c074e7b0fd8e44a0e0ba0d88046fc4cf86b0113e
mbharrat <mbharrat@gmail.com>
2018-09-18 00:49:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+        <p>is this working</p>
+        <h1>yes</h1>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
