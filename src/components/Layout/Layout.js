ee50f79aeb19a2ec98ef67288934c3fd104adee3
mbharrat <mbharrat@gmail.com>
2018-09-20 22:48:20 -0400

    finished basic functionality of app with mobile sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-		<Aux>
-			<Toolbar/>
-			<main className={classes.Content}>
-				{props.children}
-			</main>
-		</Aux>
-	);
+class Layout extends Component {
+	state  = {
+		showSideDrawer: false
+	}
+
+	sideDrawerClosedHandler = () => {
+		this.setState({showSideDrawer: false});
+	}
+	sideDrawerOpenedHandler = () => {
+		this.setState({showSideDrawer: !this.state.showSideDrawer});
+	}
+
+	render() {
+		return(
+				<Aux>
+					<Toolbar sideOpen={this.sideDrawerOpenedHandler}/>
+					<SideDrawer open={this.state.showSideDrawer} closed = {this.sideDrawerClosedHandler}/>
+					<main className={classes.Content}>
+						{this.props.children}
+					</main>
+				</Aux>
+		)
+	}
+} 
 
 export default Layout;
