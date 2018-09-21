f5f39227cf5496f5918c723abd150c29384eb135
mbharrat <mbharrat@gmail.com>
2018-09-20 23:38:35 -0400

    performance update where order summary updates only when the modal is shown

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients)
-	.map(igKey => {
-		return (<li key={igKey}>
-			<span style={{textTransform: 'capitilize'}}>{igKey}: {props.ingredients[igKey]}</span>
-			</li>);
-	});
-	return (
-		<Aux>
-			<h3>Your Order</h3>
-			<p>Delicious burger with the following ingredients:</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-			<p>Continue to Checkout?</p>
-			<Button btnType="Danger" clicked={props.cancelClicked}>CANCEL</Button>
-			<Button btnType="Success" clicked={props.continueClick}>CONTINUE</Button>
-		</Aux>
+class OrderSummary extends Component {
+	//this could be a functional component
+	/* debugging
+	componentWillUpdate(){
+		console.log("Order Summary updates");
+	}
+	*/
+	render(){
+		const ingredientSummary = Object.keys(this.props.ingredients)
+		.map(igKey => {
+			return (<li key={igKey}>
+				<span style={{textTransform: 'capitilize'}}>{igKey}: {this.props.ingredients[igKey]}</span>
+				</li>);
+		});
+		return(
+			<Aux>
+				<h3>Your Order</h3>
+				<p>Delicious burger with the following ingredients:</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p>
+					<strong>Total Price: ${this.props.price.toFixed(2)}</strong>
+				</p>
+				<p>Continue to Checkout?</p>
+				<Button btnType="Danger" clicked={this.props.cancelClicked}>CANCEL</Button>
+				<Button btnType="Success" clicked={this.props.continueClick}>CONTINUE</Button>
+			</Aux>
+		);
+	}
 
-	);
 
 
-};
+}
 
-export default orderSummary;
+export default OrderSummary;
