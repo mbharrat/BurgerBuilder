dd21d5f829d27035fe5f57796e5b61226195d296
mbharrat <mbharrat@gmail.com>
2018-10-03 14:53:10 -0400

    dynamically create component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -78,13 +78,20 @@ class ContactData extends Component {
 			});
 	}
 	render(){
+		const formElementsArray =[];
+		for(let key in this.state.orderForm){
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key]
+			});
+		}
 		let form = (
 				<form>
-					<Input elementType="..." elementConfig="..." value="..."/>
-					<Input inputtype="input" type="email" name="email" placeholder="Your Email"/>
-					<Input inputtype="input" type="text" name="street" placeholder="Your Street"/>
-					<Input inputtype="input" type="text" name="city" placeholder="Your City"/>
-					<Input inputtype="input" type="text" name="postalCode" placeholder="Your Zipcode"/>
+					{formElementsArray.map(formElement => (
+						<Input key={formElement.id} elementType={formElement.config.elementType} 
+						elementConfig={formElement.config.elementConfig} 
+						value={formElement.config.value}/>
+					))}
 					<Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
 				</form>
 		);
