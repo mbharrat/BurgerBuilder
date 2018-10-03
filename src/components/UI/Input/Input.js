ab8df5dde6b3c08cc3382e5cd2a7e171f5af2f08
mbharrat <mbharrat@gmail.com>
2018-10-03 16:45:30 -0400

    added select box to checkout

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,6 +10,16 @@ const input = (props) => {
 		case ('textArea'):
 			inputElement = <textArea className={classes.InputElement}{...props.elementConfig} value={props.value}/>;
 			break;
+		case('select'):
+			inputElement = (
+				<select className={classes.InputElement}
+				value={props.value}>
+				{props.elementConfig.options.map(option =>(
+					<option key={option.value} value={option.value}>{option.displayValue}</option>
+				))}
+				</select>
+			);
+			break;
 		default:
 			inputElement = <input className={classes.InputElement}{...props.elementConfig} value={props.value}/>;
 	}
