5152e3ca2d90fe2b13976ce2a57945f32b7a6dab
mbharrat <mbharrat@gmail.com>
2018-10-30 14:23:22 -0400

    added actions for authentication

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,5 @@
 export { addIngredient, deleteIngredient, initIngredients} from './burgerBuilder';
 
 export { purchaseBurgerStart, purchaseInit, fetchOrders } from './order';
+
+export { auth } from './auth';
